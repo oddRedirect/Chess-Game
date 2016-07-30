8261cc8a89ca8f3fff50ad0ad232769639cde0fb
walinga <matthew.walinga@gmail.com>
2016-07-30 13:21:16 -0400

    Refactored the way endgames are handled

--- ChessEngine.py
+++ ChessEngine.py
@@ -31,11 +31,60 @@ bk, bq, bb, bn, br, bp = pm.bk, pm.bq, pm.bb, pm.bn, pm.br, pm.bp
 # Gives a numerical value for how good colour's position is
 def EvaluatePosition(colour):
     evalu = 0
+    whiteMate, blackMate = pm.isMated('white'), pm.isMated('black')
 
+    # Check for draw
+    if pm.isDraw():
+        return 0
+    if colour == 'black' and whiteMate == "STALEMATE":
+        return 0
+    if colour == 'white' and blackMate == "STALEMATE":
+        return 0
+
+    # Calculate material imbalances
     for y in pm.whitepieces:
         evalu += y.value * len(y.piecelist)
+        for p in y.piecelist:
+            # Make sure pieces are safe
+            if not(y.name == 'pawn') and not(isSafe(p, 'white')):
+                evalu -= y.value/2 - 0.3
     for y in pm.blackpieces:
         evalu -= y.value * len(y.piecelist)
+        for p in y.piecelist:
+            if not(y.name == 'pawn') and not(isSafe(p, 'black')):
+                evalu += y.value/2 - 0.3
+
+    for p in wp.piecelist:
+        # Make sure pawns are safe
+        if not(isSafe(p, 'white')) and isSafe(p, 'black'):
+            evalu -= 0.4
+    for p in bp.piecelist:
+        if not(isSafe(p, 'black')) and isSafe(p, 'white'):
+            evalu += 0.4
+
+    # Checkmate is the ultimate goal
+    if isInCheck('white'):
+        evalu -= 0.5
+        if whiteMate == 'CHECKMATE':
+            evalu -= Kval
+    if isInCheck('black'):
+        evalu += 0.5
+        if blackMate == 'CHECKMATE':
+            evalu += Kval
+
+    if colour == 'black':
+        evalu *= -1
+
+    if isEndgame():
+        evalu += EvaluateEndgame(colour)
+    else:
+        evalu += EvaluateMiddleGame(colour)
+
+    return evalu
+
+
+def EvaluateMiddleGame(colour):
+    evalu = 0
 
     for y in wp.piecelist:
         # Add incentives for centred pawns
@@ -49,15 +98,13 @@ def EvaluatePosition(colour):
                     evalu += 0.35
         # Passed pawns are GREAT
         #TODO: improve alorithm for determining value of passed pawns
+        #TODO: Move this either to evalendgame or evalposition
         if y > 32 and pm.pieceatsqr(y+8) == 0:
             if y <= 54 and pm.boardlist[y+9] != id(bp) and pm.boardlist[y+7] != id(bp):
                 evalu += y/8 - 3
         # Doubled pawns are BAD
         if y < 56 and pm.boardlist[y + 8] == id(wp):
             evalu -= 0.5
-        # Don't let pawns drop off like flies
-        if not(isSafe(y, 'white')) and isSafe(y, 'black'):
-            evalu -= 0.4
 
     for y in bp.piecelist:
         if y == 35 or y == 36:
@@ -72,39 +119,25 @@ def EvaluatePosition(colour):
                 evalu -= 4 - y/8
         if y < 56 and pm.boardlist[y + 8] == id(bp):
             evalu += 0.5
-        if not(isSafe(y, 'black')) and isSafe(y, 'white'):
-            evalu += 0.4
 
     for y in wn.piecelist:
         # Add incentives for development
         if y == 18 or y == 21:
             evalu += 0.3
-        # Don't hang your knights
-        if not(isSafe(y, 'white')):
-            evalu -= 1.2
-
     for y in bn.piecelist:
         if y == 42 or y == 45:
             evalu -= 0.3
-        if not(isSafe(y, 'black')):
-            evalu += 1.2
 
     for y in wb.piecelist:
         # Fianchetto
         if y == 9 or y == 14:
             evalu += 0.35
-        # Don't hang your bishops
-        if not(isSafe(y, 'white')):
-            evalu -= 1.3
-
     for y in bb.piecelist:
         if y == 49 or y == 54:
-            evalu += 0.35
-        if not(isSafe(y, 'black')):
-            evalu += 1.3
+            evalu -= 0.35
 
     for y in wr.piecelist:
-        f = y % 8
+        f, r = y%8, y/8
         # Centralize rooks
         if f == 3 or f == 4:
             evalu += 0.25
@@ -113,28 +146,20 @@ def EvaluatePosition(colour):
             if pm.boardlist[x] == id(wp):
                 evalu -= 0.5
                 break
-        r = y/8
         # Rook on seventh rank is good
         if r == 6:
             evalu += 0.35
-        # Make sure you don't make a ROOKie mistake
-        if not(isSafe(y, 'white')):
-            evalu -= 2.1
-
     for y in br.piecelist:
-        f = y % 8
+        f, r = y%8, y/8
         if f == 3 or f == 4:
             evalu -= 0.25
         for x in [f+8, f+16, f+24, f+32, f+40, f+48]:
             if pm.boardlist[x] == id(bp):
                 evalu += 0.5
                 break
-        r = y/8
         # Rook on second rank is good
         if r == 1:
             evalu -= 0.35
-        if not(isSafe(y, 'black')):
-            evalu += 2.1
 
     # Add incentives for castling
     hs = wk.piecelist[0]
@@ -143,25 +168,6 @@ def EvaluatePosition(colour):
         evalu += 0.7
     if ls == 57 or ls == 62:
         evalu -= 0.7
-
-    # Make sure bae is safe
-    for y in wq.piecelist:
-        if not(isSafe(y, 'white')):
-           evalu -= 4.2
-    for y in bq.piecelist:
-        if not(isSafe(y, 'black')):
-            evalu += 4.2
-
-    # Checkmate is the ultimate goal
-    if isInCheck('white'):
-        evalu -= 0.5
-        if pm.isMated('white') == 'CHECKMATE':
-            evalu -= Kval
-
-    if isInCheck('black'):
-        evalu += 0.5
-        if pm.isMated('black') == 'CHECKMATE':
-            evalu += Kval
 
     if colour == 'black':
         evalu *= -1
@@ -283,104 +289,35 @@ def isEndgame():
     return False
 
 
+def EvaluateEndgame(colour):
+    evalu = 0
+    if colour == 'white':
+        king, oppKing = wk.piecelist[0], bk.piecelist[0]
+    else:
+        king, oppKing = bk.piecelist[0], wk.piecelist[0]
+
+    # Try to limit the enemy king's movement
+    a = len(PieceMovement(oppKing))
+
+    if a < 2:
+        evalu += 3
+    elif a < 3:
+        evalu += 1.5
+    elif a < 4:
+        evalu += 1
+    elif a < 5:
+        evalu += 0.7
+    elif a < 6:
+        evalu += 0.5
+
+    if hasOpposition(king, oppKing):
+        evalu += 0.5
+
+    return evalu
+    
+
 # Returns a move to maybe get closer to destroying the player
 def BasicMates(colour):
-    if colour == 'white':
-        kingpos, opp_pos = wk.piecelist[0], bk.piecelist[0]
-        rooks = wr.piecelist + wq.piecelist
-        queen = wq.piecelist
-    elif colour == 'black':
-        kingpos, opp_pos = bk.piecelist[0], wk.piecelist[0]
-        rooks = br.piecelist + bq.piecelist
-        queen = bq.piecelist
-    
-    # 2 Rook Checkmate
-    if len(rooks) >= 2:
-        kf = opp_pos % 8
-        kr = opp_pos / 8
-        for y in rooks:
-            for x in PieceMovement(y):
-                if not(isSafe(y, colour)) and isSafe(x, colour):
-                    if y / 8 == kr + 1:
-                        if x / 8 != kr+1 or x%8 == kf+2 or x%8 == kf-2:
-                            continue
-                    return y, x
-        if rooks[0] / 8 == kr + 1 or rooks[1] / 8 == kr + 1:
-            for y in rooks:
-                for x in PieceMovement(y):
-                    if isSafe(x, colour) and x / 8 == kr:
-                        if y / 8 != kr + 1 or (rooks[0]/8 == kr+1 and rooks[1]/8 == kr+1):
-                            return y, x
-        else:
-            for y in rooks:
-                for x in PieceMovement(y):
-                    if isSafe(x, colour) and x / 8 == kr + 1:
-                        return y, x
-
-    # King and Queen Checkmate
-    elif len(queen) > 0:
-        start = queen[0]
-        k, opr = hasOpposition(kingpos, opp_pos)
-        if k:
-            for end in PieceMovement(start):
-                if opr == "file" and end%8 == k and isSafe(y, colour):
-                    return start, end
-                elif opr == "rank" and end/8 == k and isSafe(y, colour):
-                    return start , end
-        elif opp_pos + 15 == kingpos or opp_pos + 17 == kingpos:
-            for y in PieceMovement(start):
-                if y == opp_pos + 8:
-                    return start, y
-        elif opp_pos - 15 == kingpos or opp_pos - 17 == kingpos:
-            for y in PieceMovement(start):
-                if y == opp_pos - 8:
-                    return start, y
-        elif opp_pos - 10 == kingpos or opp_pos + 6 == kingpos:
-            for y in PieceMovement(start):
-                if y == opp_pos - 1:
-                    return start, y
-        elif opp_pos - 6 == kingpos or opp_pos + 10 == kingpos:
-            for y in PieceMovement(start):
-                if y == opp_pos + 1:
-                    return start, y
-        else:
-            v = getOpposition(kingpos, opp_pos)
-            if v:
-                return kingpos, v
-
-    # King and Rook Checkmate
-    elif len(rooks) == 1:
-        k, opr = hasOpposition(kingpos, opp_pos)
-        kf = opp_pos % 8
-        kr = opp_pos / 8
-        if k:
-            start = rooks[0]
-            for end in PieceMovement(start):
-                if opr == "file" and end%8 == k and isSafe(y, colour):
-                    return start, end
-                elif opr == "rank" and end/8 == k and isSafe(y, colour):
-                    return start , end
-        elif rooks[0] / 8 != kr + 1 or not(isSafe(rooks[0], colour)):
-            start = rooks[0]
-            for y in PieceMovement(start):
-                if y/8 == kr+1 and isSafe(y, colour):
-                    if start / 8 == kr + 1:
-                        if y / 8 != kr+1 or y%8 == kf+2 or y%8 == kf-2:
-                            continue
-                    return start, y
-        elif kingpos / 8 > kr + 2:
-            for y in PieceMovement(kingpos):
-                if y == kingpos - 8:
-                    return kingpos, kingpos - 8
-        for y in PieceMovement(kingpos):
-            if y / 8 == kr + 2:
-                if opp_pos + 15 == y or opp_pos + 17 == y or opp_pos + 16 == y:
-                    return kingpos, y
-        for y in PieceMovement(kingpos):
-            if y / 8 == kr + 2:
-                if opp_pos + 14 == y or opp_pos + 18 == y:
-                    return kingpos, y
-                    
     default = FindBest(colour, 2)
     return default.movestart, default.moveend
                 
